3ada2313adb8a986de542c39dfd9e32ddace767f
Cristian-ZG <zuniga.guti.cristian@gmail.com>
2024-05-12 22:55:16 -0500

    estructura inicial del proyecto.

---
+++ www/js/index.js
@@ -0,0 +1,23 @@
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
+
+window.fn = {};
+
+window.fn.open = function() {
+  var menu = document.getElementById('menu');
+  menu.open();
+};
+
+window.fn.load = function(page) {
+  var content = document.getElementById('content');
+  var menu = document.getElementById('menu');
+  content.load(page)
+    .then(menu.close.bind(menu));
+};
