8f41b8747a639a320ff408dd7b91ee56f92eb994
Cristian-ZG <zuniga.guti.cristian@gmail.com>
2024-05-27 20:43:25 -0500

    Creacion de bd SQLite y logica para agregar, leer y borrar tareas.

--- www/js/index.js
+++ www/js/index.js
@@ -1,80 +1,102 @@
-
 document.addEventListener('deviceready', onDeviceReady, false);
 
+var db;
+
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
 
-window.fn = {};
+    db = window.sqlitePlugin.openDatabase({name: 'todo.db', location: 'default'});
 
-window.fn.open = function() {
-  var menu = document.getElementById('menu');
-  menu.open();
-};
-
-window.fn.load = function(page) {
-  var content = document.getElementById('content');
-  var menu = document.getElementById('menu');
-  content.load(page)
-    .then(menu.close.bind(menu));
-};
-
-function addTask(){
-window.location.href = 'addTarea.html';
-}
-
-//------------------------------> FUNCIONALIDAD <-----------------------------------------------
-
-// Evento del elemento ons-fab para navegar a la pagina add-tarea.html
-document.addEventListener('init', function(event) {
-  var page = event.target;
-
-  if (page.id === 'home') {
-    // Obtener referencia al alemento ons-fab que se encuentra en home.html
-    var fabButton = document.getElementById('fab-button');
-
-    // Agregar evento de clic al elemento ons-fab
-    fabButton.addEventListener('click', function() {
-      // Navegar a la página add-tarea.html
-      myNavigator.pushPage('add-tarea.html');
+    db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, date TEXT, time TEXT)', [], function(tx, res) {
+            console.log('Table created successfully');
+        }, function(tx, error) {
+            console.log('CREATE TABLE error: ' + error.message);
+        });
     });
 
-    // Manejar eliminación de tareas
-    var taskList = document.getElementById('lista');
-    taskList.addEventListener('click', function(event) {
-       if (event.target.closest('.ion-ios-trash')) {
-           event.target.closest('ons-list-item').remove();
-       }
-    });
+    loadTasks();
+}
 
-  } else if (page.id === 'add-tarea') {
-    var saveButton = page.querySelector('ons-toolbar-button[component="button/save-task"]');
-    saveButton.onclick = function() {
-        var titleInput = document.querySelector('#title-input');
-        var taskDate = document.querySelector('#taskDate');
-        var taskTime = document.querySelector('#taskTime');
-
-        if (titleInput.value) {
+function loadTasks() {
+    db.transaction(function(tx) {
+        tx.executeSql('SELECT * FROM tasks', [], function(tx, resultSet) {
             var list = document.querySelector('#lista');
-            var newItem = document.createElement('ons-list-item');
-            newItem.innerHTML = `
-               <label class="left">
-                   <ons-checkbox input-id="check-${Date.now()}"></ons-checkbox>
-               </label>
-               <label for="check-${Date.now()}" class="center">
-                   ${titleInput.value}
-               </label>
-               <label class="right"><ons-icon icon="ion-ios-trash"></ons-icon></label>
-            `;
-            list.appendChild(newItem);
+            list.innerHTML = ''; // Clear current list
+            for (var i = 0; i < resultSet.rows.length; i++) {
+                var task = resultSet.rows.item(i);
+                var newItem = document.createElement('ons-list-item');
+                newItem.innerHTML = `
+                    <label class="left">
+                        <ons-checkbox input-id="check-${task.id}"></ons-checkbox>
+                    </label>
+                    <label for="check-${task.id}" class="center">
+                        ${task.title}
+                    </label>
+                    <label class="right">
+                        <ons-icon icon="ion-ios-trash" data-id="${task.id}"></ons-icon>
+                    </label>
+                `;
+                list.appendChild(newItem);
+            }
+        }, function(tx, error) {
+            console.log('SELECT error: ' + error.message);
+        });
+    });
+}
 
-            document.querySelector('#myNavigator').popPage();
-        } else {
-            ons.notification.alert('Por favor ingresa un título para la tarea.');
-        }
-    };
-  }
+document.addEventListener('init', function(event) {
+    var page = event.target;
+
+    if (page.id === 'home') {
+        var fabButton = document.getElementById('fab-button');
+        fabButton.addEventListener('click', function() {
+            document.querySelector('#myNavigator').pushPage('add-tarea.html');
+        });
+
+        var taskList = document.getElementById('lista');
+        taskList.addEventListener('click', function(event) {
+            if (event.target.closest('.ion-ios-trash')) {
+                var taskId = event.target.getAttribute('data-id');
+                deleteTask(taskId);
+                event.target.closest('ons-list-item').remove();
+            }
+        });
+
+    } else if (page.id === 'add-tarea') {
+        var saveButton = page.querySelector('ons-toolbar-button[component="button/save-task"]');
+        saveButton.addEventListener('click', function() {
+            var titleInput = document.querySelector('#title-input');
+            var taskDate = document.querySelector('#taskDate');
+            var taskTime = document.querySelector('#taskTime');
+
+            if (titleInput.value) {
+                addTask(titleInput.value, taskDate.value, taskTime.value);
+                document.querySelector('#myNavigator').popPage();
+            } else {
+                ons.notification.alert('Por favor ingresa un título para la tarea.');
+            }
+        });
+    }
 });
+
+function addTask(title, date, time) {
+    db.transaction(function(tx) {
+        tx.executeSql('INSERT INTO tasks (title, date, time) VALUES (?, ?, ?)', [title, date, time], function(tx, res) {
+            console.log('Task inserted with ID: ' + res.insertId);
+            loadTasks();
+        }, function(tx, error) {
+            console.log('INSERT error: ' + error.message);
+        });
+    });
+}
+
+function deleteTask(id) {
+    db.transaction(function(tx) {
+        tx.executeSql('DELETE FROM tasks WHERE id = ?', [id], function(tx, res) {
+            console.log('Task deleted');
+        }, function(tx, error) {
+            console.log('DELETE error: ' + error.message);
+        });
+    });
+}
